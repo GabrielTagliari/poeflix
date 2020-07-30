b3e58797f03a762b3413c6f59641840d6fb517ee
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2020-07-29 21:34:40 -0300

    Lesson 2 - Creating pages and improving carrousel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import RegisterVideo from './pages/register/Video'
+import RegisterCategory from './pages/register/Category';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={RegisterVideo} exact />
+      <Route path="/cadastro/categoria" component={RegisterCategory} exact />
+      <Route component={() => (<div>404</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
