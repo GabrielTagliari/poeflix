a5c93267926c0ef3b46b2394150c1a17cb463854
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2020-08-01 15:21:47 -0300

    Adding heroku url

--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -26,7 +26,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://poeflix.herokuapp.com/categorias';
 
     fetch(URL).then(async (response) => {
       const obj = await response.json();
